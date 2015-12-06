476712f9a44e63ee0d9fd7674a3ad9b682b3becb
Harsh Verma <hverma@seas.upenn.edu>
2015-12-05 23:54:55 -0500

    adding pos files

---
+++ project_lm.py
@@ -0,0 +1,314 @@
+from __future__ import division
+from nltk.tokenize import sent_tokenize, word_tokenize
+#from nltk import StanfordPOSTagger
+from collections import defaultdict
+import math
+import subprocess
+import os
+import csv
+import operator
+import re
+from sets import Set
+from collections import OrderedDict, Counter
+import linecache
+from svmutil import *
+import random
+
+
+
+
+
+def flatten(listoflists):
+    flat_list=[]
+    for inner_list in listoflists:
+        flat_list.extend(inner_list)
+    return flat_list
+
+
+def get_all_files(directory):
+    list_entries=os.listdir(directory)
+    list_files=[entry for entry in list_entries if os.path.isfile(os.path.join(directory,entry))]
+    return list_files
+
+def get_fullpath_files(directory):
+    list_entries=os.listdir(directory)
+    list_files=[os.path.join(directory,entry) for entry in list_entries if os.path.isfile(os.path.join(directory,entry))]
+    return list_files
+
+def parse_input_mft(train_file):
+    fp = open(train_file,"r")
+    list_excerpts_label = []
+    for line in fp:
+        tokens = line.rsplit('\t',1)
+        list_excerpts_label.append((tokens[0], int(tokens[1].strip())))
+    fp.close()
+    list_excerpts = [tuple_entry[0] for tuple_entry in list_excerpts_label]
+    excerpts_list_words = []
+    for excerpt in list_excerpts:
+        excerpt.strip()
+        word_list = word_tokenize(excerpt.decode('utf8'))
+        excerpts_list_words.append(word_list)
+    excerpts_list_words = flatten(excerpts_list_words)
+    word_dictionary = defaultdict(int)
+    for token in excerpts_list_words:
+        word_dictionary[token]+=1
+    counter_words = Counter(word_dictionary)
+    list_words = []
+    for k,v in counter_words.most_common(1000):
+        list_words.append(k)
+    return list_excerpts_label, list_excerpts, list_words
+
+
+def parse_input(train_file):
+    fp = open(train_file,"r")
+    list_excerpts_label = []
+    for line in fp:
+        tokens = line.rsplit('\t',1)
+        list_excerpts_label.append((tokens[0], int(tokens[1].strip())))
+    fp.close()
+    return list_excerpts_label
+
+def parse_test_input(testfile):
+    fp = open(testfile,"r")
+    list_excerpts_label = []
+    for line in fp:
+        list_excerpts_label.append((line, 0))
+    fp.close()
+    return list_excerpts_label
+
+def get_positive_examples(list_excerpts_label,outfile):
+    fp = open(outfile,"w+")
+    for excerpt_label in list_excerpts_label:
+        if(excerpt_label[1] == 1):
+            fp.write(excerpt_label[0]+ "\n")
+    fp.close()
+
+
+def get_negative_examples(list_excerpts_label,outfile):
+    fp = open(outfile,"w+")
+    for excerpt_label in list_excerpts_label:
+        if(excerpt_label[1] == 0):
+            fp.write(excerpt_label[0]+ "\n")
+    fp.close()
+
+
+
+def load_topic_words(topic_file, n):
+    fp = open(topic_file,'r')
+    list_tuples = []
+    for line in fp:
+        tokens = line.split(" ")
+        if float(tokens[1].strip()) >= 10.0:
+            list_tuples.append((tokens[0], float(tokens[1].strip())))
+    list_tuples = sorted(list_tuples, key = lambda tuple_entry: tuple_entry[1], reverse=True)
+    if n < len(list_tuples):
+        tuple_topn = list_tuples[:n]
+    else:
+        tuple_topn = list_tuples
+    list_words = [tuple_entry[0] for tuple_entry in tuple_topn]
+    fp.close()
+    return list_words
+
+def get_vocab_dict(list_words):
+    vocab_list = sorted(list_words)
+    vocab_index = {}
+    for i in range(1,(len(vocab_list) + 1)):
+        vocab_index[vocab_list[i-1]] = i
+    return vocab_index
+
+
+def generate_test_dict(excerpt,vocab_dict):
+    excerpt_count_dict = defaultdict(int)
+    words_excerpt = word_tokenize(excerpt.decode('utf8'))
+    for word in words_excerpt:
+        excerpt_count_dict[word]+=1
+    dict_index_count = {}
+    for word in excerpt_count_dict:
+        if word in vocab_dict:
+            dict_index_count[vocab_dict[word]] =  excerpt_count_dict[word]/len(excerpt)
+    return dict_index_count
+
+def generate_count_dict(excerpt,vocab_dict):
+    excerpt_count_dict = defaultdict(int)
+    words_excerpt = word_tokenize(excerpt.decode('utf8'))
+    for word in words_excerpt:
+        excerpt_count_dict[word]+=1
+    dict_index_count = {}
+    for word in excerpt_count_dict:
+        if word in vocab_dict:
+            dict_index_count[vocab_dict[word]] = excerpt_count_dict[word]
+    return dict_index_count
+
+def train_test_model(train_datafile, test_datafile):
+        y,x = svm_read_problem(train_datafile)
+        test_y, test_x = svm_read_problem(test_datafile)
+        model_train = svm_train(y,x, '-t 0 -e .01 -m 1000 -h 0')
+        p_labs, p_acc, p_vals = svm_predict(test_y, test_x, model_train)
+        return p_labs, p_acc, p_vals
+
+def train_test_model_final(train_datafile, test_datafile):
+        y,x = svm_read_problem(train_datafile)
+        test_y, test_x = svm_read_problem(test_datafile)
+        model_train = svm_train(y,x, '-t 0 -e .01 -m 1000 -h 0')
+        p_labs, p_acc, p_vals = svm_predict([0]*len(test_x), test_x, model_train)
+        return p_labs
+
+
+
+def train_crossvalidation(train_datafile, testlinenos):
+    train_lines = set(range(1,12114))
+    train_lines.difference_update(testlinenos)
+    fp_test = open("svm_test.txt","w+")
+    fp_train = open("svm_train.txt","w+")
+    for line in testlinenos:
+	cur_line = linecache.getline(train_datafile, line)
+        fp_test.write(cur_line)
+    for line in train_lines:
+        cur_line = linecache.getline(train_datafile, line)
+ 	fp_train.write(cur_line)
+    fp_train.close()
+    fp_test.close()
+    p_labs, p_acc, p_vals = train_test_model("svm_train.txt","svm_test.txt")
+    return p_acc
+
+
+def postagstring(inputtotag, st):
+    arrayrepresentation = word_tokenize(inputtotag)
+    posarrayform = st.tag(arrayrepresentation)
+    return posarrayform
+
+def writetodoc(outfile, posarray, intex):
+    trueposarray = [wordpair[1] for wordpair in posarray]
+    for pos in trueposarray:
+        outfile.write(pos)
+        outfile.write(" ")
+    if intex > -1:
+        outfile.write('\t ' + intex)
+    outfile.write('\n')
+
+def postagdocument(inputdocument, outfile):
+    st = StanfordPOSTagger('english-bidirectional-distsim.tagger') 
+    outputfile = open(outfile, 'w')
+    f = open(inputdocument, 'r')
+    arrayofexcerpts = f.readlines()
+    f.close()
+    for i in range (0, len(arrayofexcerpts)):
+        # temp = arrayofexcerpts[i].rsplit('\t',1)
+        posarray = postagstring(arrayofexcerpts[i].decode('utf8'), st)
+        # print posarray[0][1]
+        # temp.append("")
+        # temp.append("")
+        writetodoc(outputfile, posarray, -1)
+    outputfile.close()
+
+
+def srilm_preprocess(raw_text, temp_file):
+    sentence_list = sent_tokenize(raw_text)
+    tmp_fp = open(temp_file, 'w')
+    for sentence in sentence_list:
+        tmp_fp.write(sentence.encode('utf8') + '\n')
+
+def srilm_bigram_models(input_file, output_dir):
+    fp = open(input_file)
+    raw_text = fp.read().decode('utf8')
+    bigram_lm_file = os.path.join(output_dir, os.path.basename(input_file) + ".bi.lm")
+    srilm_preprocess(raw_text,'./temp_file')
+    subprocess.call(["/home1/c/cis530/srilm/ngram-count","-text","./temp_file","-order","3","-kndiscount","-lm",bigram_lm_file])
+
+def find_ppl(model_file, test_file):
+    result = subprocess.check_output(["/home1/c/cis530/srilm/ngram","-lm", model_file,"-ppl", test_file])
+    list_result = result.split("=")
+    list_ppl_value = [s for s in list_result if 'ppl1' in s]
+    final_ppl = 0.0
+    if (len(list_ppl_value) >0):
+        list_final_ppl = list_ppl_value[0].split(" ")
+        final_ppl = float(list_final_ppl[1])
+    return final_ppl 
+
+
+def predict_authorship(excerpt, positivemodelfile, negativemodelfile):
+    list_sentences = sent_tokenize(excerpt)
+    fp = open("predict_temp_file.txt", "w+")
+    for sentence in list_sentences:
+        fp.write(sentence + "\n")
+    fp.close()
+    ppl_positive = find_ppl(positivemodelfile, "predict_temp_file.txt")
+    ppl_negative = find_ppl(negativemodelfile, "predict_temp_file.txt")
+    if ppl_negative <= ppl_positive:
+        return False
+    else:
+        return True
+
+def print_result_testfile(testfile, outputfile, positivemodelfile, negativemodelfile):
+    fp_input = open(testfile, "w+")
+    fp_output = open(outputfile, "w+")
+    for excerpt in fp_input:
+        if predict_authorship(excerpt, positivemodelfile, negativemodelfile):
+            fp_output.write("1" + "\n")
+        else:
+            fp_output.write("0" + "\n")
+    fp_input.close()
+    fp_output.close()
+
+
+
+
+
+
+
+	    
+
+if __name__=="__main__":
+    #java_path = "C:/Program Files/Java/jdk1.8.0_65/bin/java.exe"
+    #os.environ['JAVAHOME'] = java_path
+    #postagdocument("train_negative", "pos_negative")
+    #data = parse_input("data/project_articles_train")
+    #data = parse_test_input("data/project_articles_test")
+    #get_positive_examples(data, "train_positive")
+    #topic_words = load_topic_words("topic_words.ts", 1000)
+    # list_dummy,list_dummy1,topic_words = parse_input_mft("data/project_articles_train")
+    # vocab_dict = get_vocab_dict(topic_words)
+    # list_label_features = []
+    # for tuple_entry in data:
+    #     list_label_features.append((tuple_entry[1], generate_count_dict(tuple_entry[0], vocab_dict)))
+    # svm_file = open("file_svm_test.txt", "w+")
+    # for instance in list_label_features:
+    #     svm_file.write(str(instance[0]) + " ")
+    #     od = OrderedDict(sorted(instance[1].items()))
+    #     for key in od:
+    #         svm_file.write(str(key) + ":" + str(od[key])  + " ")
+    #     svm_file.write("\n")
+	#data_test = parse_input("data/project_articles_test")
+    #get_positive_examples(data, "train_positive")
+    #get_negative_examples(data, "train_negative")
+    #print train_test_model("file_svm_train.txt", "file_svm_test.txt")
+    # lines_tested = set()
+    # left_set = set(range(1,12114))
+    # total_acc = 0
+    # counter = 0
+    # while(True):
+    #     if len(left_set) < 1000:
+    #         counter+=1
+    #         total_acc+=train_crossvalidation("file_svm_train_one.txt", left_set)[0]
+    #         break
+    #     else:
+    #         counter+=1
+    #         current_set = set(random.sample(list(left_set), 1000))
+    #         lines_tested.union(current_set)
+    #         left_set.difference_update(current_set)
+    #         total_acc+=train_crossvalidation("file_svm_train_one.txt", current_set)[0]
+    # print total_acc/counter
+    #srilm_bigram_models("train_positive","/home1/h/hverma/Project_CIS539/cis530_project/language_models")
+    #srilm_bigram_models("train_negative","/home1/h/hverma/Project_CIS539/cis530_project/language_models")
+    list_labels = train_test_model_final("file_svm_train.txt","file_svm_test.txt")
+    fp_result = open("testy.txt", "w+")
+    for label in list_labels:
+        fp_result.write(str(int(label)) + "\n")
+    fp_result.close()
+
+
+
+
+
+    
+    
